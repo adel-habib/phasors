44222f0c42b6b2f15b97708240bf05874e973b75
adel <adill.u@outlook.com>
2021-10-28 19:43:11 +0200

    Versor and the 'a' operator

--- phasors.py
+++ phasors.py
@@ -2,6 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from numpy import linspace, cos, sin, pi 
 
+
+def cart2pol(z):
+    x = z.real
+    y = z.imag
+    mag = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return Phasor(mag,phi)
+
+def pol2cart(mag, phi):
+    x = mag * np.cos(phi)
+    y = mag * np.sin(phi)
+    return(x + 1j*y)     
+
 class Phasor:
     def __init__(self,mag,phase):
         self.mag = mag
@@ -10,27 +23,78 @@ class Phasor:
         self.cartesian = self.get_cartesian()
         self.real = self.cartesian.real
         self.imag = self.cartesian.imag
-        
+    
+    # Overloading + - * / ** operators 
     def __str__(self):
-         return str(self.mag) + '∠' + str(self.angle)
+         return str(round(self.mag)) + '∠' + str(round(self.angle)) + "°"
     def __repr__(self):
-         return str(self.mag) + '∠' + str(self.angle)
+         return str(round(self.mag)) + '∠' + str(round(self.angle)) + "°"
     def __mul__(self,other):
-        if isinstance(other, (int, float, complex)) and not isinstance(x, bool):
+        if isinstance(other, (int, float)) and not isinstance(x, bool):
             return Phasor(other*self.mag,self.phase)
         elif isinstance(other, Phasor):
             return Phasor(self.mag*other.mag,self.phase+other.phase)
+        elif isinstance(other,complex):
+            z = self.cartesian * other
+            return cart2pol(z)
+
         
     def __rmul__(self,other):
-        if isinstance(other, (int, float, complex)) and not isinstance(x, bool):
+        if isinstance(other, (int, float)) and not isinstance(x, bool):
             return Phasor(other*self.mag,self.phase)
         elif isinstance(other, Phasor):
             return Phasor(self.mag*other.mag,self.phase+other.phase)
-        
+        elif isinstance(other,complex):
+            z = self.cartesian * other
+            return cart2pol(z)
+    
+    def __add__(self,other):
+        if isinstance(other,(int,float,complex)):
+            z = self.cartesian + other
+            return cart2pol(z)
+        elif isinstance(other,Phasor):
+            z = self.cartesian + other.cartesian
+            return cart2pol(z)
+
+    def __pow__(self,other):
+        if(other,isinstance(other,int)):
+            return Phasor(self.mag**other,self.phase*other)
+
+    def __truediv__(self,other):
+        if isinstance(other,(int,float)):
+            return Phasor(self.mag/other,self.phase)
+        elif isinstance(other,complex):
+            div = cart2pol(other)
+            return Phasor(self.mag/div.mag,self.phase-div.phase)
+        elif isinstance(other,Phasor):
+            return Phasor(self.mag/other.mag,self.phase-other.phase)
+    def __rtruediv__(self,other):
+        if isinstance(other,(int,float)):
+            return Phasor(other/self.mag,-self.phase)
+        elif isinstance(other,complex):
+            div = cart2pol(other)
+            return Phasor(div.mag/self.mag,div.phase-self.phase)
+        elif isinstance(other,Phasor):
+            return Phasor(other.mag/self.mag,other.phase-self.phase)
+
+    def __sub__(self,other):
+        if isinstance(other,(int,float,complex)):
+            z = self.cartesian - other
+            return cart2pol(z)
+        elif isinstance(other,Phasor):
+            z = self.cartesian - other.cartesian
+            return cart2pol(z)
+
+    def pol2cart(rho, phi):
+        x = rho * np.cos(phi)
+        y = rho * np.sin(phi)
+        return(x + 1j*y)     
+
+
     def get_cartesian(self):
         m = self.mag
         p = self.phase
-        return (m*cos(p) + 1j*sin(p))
+        return m*(cos(p) + 1j*sin(p))
     
     def get_conjugate(self):
         return Phasor(self.mag,-self.phase)
@@ -54,3 +118,26 @@ class Phasor:
         col = plt.gca().lines[-1].get_color()
         plt.annotate('', xy=(0, 0),xycoords='data',xytext=(x[1],y[1]),textcoords='data',arrowprops=dict(arrowstyle='<|-',color=col,mutation_scale=25,lw=2))
         plt.plot(-x,-y,alpha=0)
+
+class Versor(Phasor):
+        def __init__(self,phase):
+            mag = 1
+            self.mag = mag
+            self.phase = phase 
+            self.angle = round(phase*180/pi,3)
+            self.cartesian = self.get_cartesian()
+            self.real = self.cartesian.real
+            self.imag = self.cartesian.imag
+
+
+v = Phasor(10,pi/3)
+a = Versor(2*pi/3)
+v2 = v *a
+v.init_axis()
+v.plot()
+v2.plot()
+v3 = v2 * a
+
+v3.plot()
+plt.show()
+print(type(v3))
